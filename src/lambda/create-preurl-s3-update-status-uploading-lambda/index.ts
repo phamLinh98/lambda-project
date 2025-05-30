c99fb69d6757f6f2ed2aff538ef966c8a7c428ea
phamLinh98 <linhthusinh98@gmail.com>
2025-05-30 21:51:25 +0900

    Add logging for Lambda PreURL call

--- src/lambda/create-preurl-s3-update-status-uploading-lambda/index.ts
+++ src/lambda/create-preurl-s3-update-status-uploading-lambda/index.ts
@@ -10,6 +10,7 @@ import { getSecretOfKey } from "../get-secret-key-from-manager";
 
 export const handler = async (event: any) => {
   try {
+    console.log("Call Lambda PreURL >>");
     // Get the bucket name and table name from Secrets Manager
     const bucketName = (await getSecretOfKey("bucketCsvName")) as any;
     console.log("bucketName >>>", bucketName);
