e63a62016fa843e6701cb0066a29bf9663e4449d
phamLinh98 <linhthusinh98@gmail.com>
2025-06-24 00:57:07 +0900

    Add debugging configuration for test.js and enhance logging in Lambda handler

--- mock-aws/mock.ts
+++ mock-aws/mock.ts
@@ -11,7 +11,7 @@ import { handler } from "../src/lambda/create-preurl-s3-update-status-uploading-
         receiptHandle: "VALID_RECEIPT_HANDLE_FROM_RECEIVE_MESSAGE",
         s3: {
           bucket: {
-            name: "1fab83f3-5260-43d7-afb2-f33cc596896c "
+            name: "1fab83f3-5260-43d7-afb2-f33cc596896c"
           },
           object: {
             key: "1fab83f3-5260-43d7-afb2-f33cc596896c.csv"
--- src/lambda/create-preurl-s3-update-status-uploading-lambda/index.ts
+++ src/lambda/create-preurl-s3-update-status-uploading-lambda/index.ts
@@ -10,39 +10,49 @@
 // import { getSecretOfKey } from "../../../localstack/mock-secret-key";
 // import { connectToS3Bucket } from '../../../localstack/mock-s3';
 // import { getInstanceDynamoDB, updateTableInDynamoDB } from "../../../localstack/mock-dynamo-db";
-import { getInstanceDynamoDB } from '../../../mock-aws/mock-db';
-import { connectToS3Bucket } from '../../../mock-aws/mock-s3';
-import { getSecretOfKey } from '../../../mock-aws/mock-secret';
+import { getInstanceDynamoDB } from "../../../mock-aws/mock-db";
+import { connectToS3Bucket } from "../../../mock-aws/mock-s3";
+import { getSecretOfKey } from "../../../mock-aws/mock-secret";
 import { updateTableInDynamoDB } from "../create-update-detele-search-dynamo-sqs-s3/connectAndUpdateDynamoDb";
-import {
-  createPreUrlUpdateS3,
-} from "../create-update-detele-search-dynamo-sqs-s3/connectAndUpdateS3";
+import { createPreUrlUpdateS3 } from "../create-update-detele-search-dynamo-sqs-s3/connectAndUpdateS3";
 
 export const handler = async (event: any) => {
   try {
-    console.log('event', event);
+    console.log("🔍 Check/find error point 1");
+    console.log("event", event);
     // 4.1 Lấy bucketName bucketCsvName
     const bucketName = (await getSecretOfKey("bucketCsvName")) as any;
 
     // 4.2 Lấy tên bảng uploadCsvTableName
     const uploadCsvTable = (await getSecretOfKey("uploadCsvTableName")) as any;
+    console.log("🔍 Check/find error point 2");
     const s3Client = await connectToS3Bucket();
     const dynamoDB = await getInstanceDynamoDB();
 
+    console.log("BEBUG #22556(1):");
     // 4.3 Tạo hàm generateUUID để tạo ra một UUID duy nhất
     const generateUUID = () => {
-      return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      console.log("BEBUG #22556(2):");
+      const result = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
         /[xy]/g,
         function (c) {
+          console.log("BEBUG #22556(2.1):", c);
           const r = (Math.random() * 16) | 0;
+          console.log("BEBUG #22556(2.2):", r);
           const v = c === "x" ? r : (r & 0x3) | 0x8;
+          console.log("BEBUG #22556(2.3):", v);
           return v.toString(16);
         }
       );
+      console.log("BEBUG #22556(2.4):", result);
+      return result;
     };
 
     // 4.4 Tạo tên tệp bằng UUID với hàm generateUUID
     const fileName = generateUUID();
+    console.log("BEBUG #22556(3):", fileName);
+
+    console.log("🔍 Check/find error point 3");
 
     // 4.5 Cập nhật trạng thái của bảng DynamoDB thành "Uploading"
     await updateTableInDynamoDB(
@@ -59,6 +69,7 @@ export const handler = async (event: any) => {
     const timeExpired = 3600;
 
     // 4.8 Tạo Pre-signed URL để cập nhật tệp CSV vào S3 bucket
+    console.log("🔍 Check/find error point 4");
     const data = await createPreUrlUpdateS3(
       s3Client,
       bucketName,
@@ -67,10 +78,11 @@ export const handler = async (event: any) => {
       fileName
     );
 
+    console.log("🔍 Check/find error point 5");
     // // 4.9 Trả về Pre-signed URL đã tạo
     return data;
   } catch (error) {
-     console.error("Call Lambda Fail");
+    console.error("Call Lambda Fail");
     return {
       statusCode: 500,
       body: JSON.stringify({ message: "Call Lambda PreURL fail" }),
